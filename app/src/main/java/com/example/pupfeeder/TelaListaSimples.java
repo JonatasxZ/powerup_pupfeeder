528f0a1fffca5c144bddb9a6f2f91bd09c9d705d
JONATAS PEREIRA DO NASCIMENTO <202109069934@alunos.estacio.br>
2023-11-03 20:44:05 -0300

    Tela de cadastro de lista

--- app/src/main/java/com/example/pupfeeder/TelaListaSimples.java
+++ app/src/main/java/com/example/pupfeeder/TelaListaSimples.java
@@ -2,13 +2,18 @@ package com.example.pupfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class TelaListaSimples extends AppCompatActivity {
 
     ListView listasimples;
+
+    Button novalista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,15 @@ public class TelaListaSimples extends AppCompatActivity {
         setContentView(R.layout.activity_tela_lista_simples);
 
         listasimples = (ListView) findViewById(R.id.ListViewListaSimples);
+
+        novalista = (Button) findViewById(R.id.buttonListaNova);
+
+        novalista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TelaListaSimples.this, TelaCadastroListas.class));
+            }
+        });
 
         String [] lista = new String[]{"Simples", "Comum", "Rara", "Limitada"};
 
